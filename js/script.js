7e284351d10056ec94de4979f0231d82f1af12fb
ErickyAll <erick227.ea21@gmail.com>
2022-07-04 18:06:33 -0300

    Add JQuery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-function abreFechaMenu() {
-  // navbar -tag nav
-  let navbar = document.querySelector('.menu')
-  navbar.classList.toggle('mostraEscondeMenu')
-  // console.log(navbar);
-}
+$(document).ready(function () {
+  $('.menu-bars').click(function () {
+    $('.menu').toggleClass('mostraEscondeMenu')
+  })
+
+  $('.btn-price').click(function () {
+    alert('Produto indisponivel :/')
+  })
+})
+
+// function abreFechaMenu() {
+//   // navbar -tag nav
+//   let navbar = document.querySelector('.menu')
+//   navbar.classList.toggle('mostraEscondeMenu')
+//   // console.log(navbar);
+// }
